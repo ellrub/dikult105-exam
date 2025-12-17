5d02a53c09a43007da5eb03c2f02e300ceef38ab
Ruben Nienhuis <ellefsen88@gmail.com>
2025-12-17 09:29:47 +0100

    Fix issues with projects review
    Fix issue that prevents the carousel from switching between projects when clicking on the corresponding button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,39 +2,33 @@ import { useState } from 'react'
 import './header.css'
 import './floatingDots.css'
 
-const buttons = document.querySelectorAll('.header__project-nav button');
-const sections = {
-    0: document.querySelector('.header__section-home'),
-    1: document.querySelector('.header__project-whitespace'),
-    2: document.querySelector('.header__project-snapshot'),
-    3: document.querySelector('.header__project-insanity')
-}
-
-let currentSelection = 0;
-
-buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        sections[currentSelection].style.display = 'none';
-        const targetIndex = index + 1;
-        sections[targetIndex].style.display = 'flex';
-        sections[targetIndex].style.animation = 'slideInFromRight 0.8s ease-out';
-
-        currentSelection = targetIndex;
-    });
-});
-
 function Header() {
+    const [activeSelection, serActiveSelection] = useState(0);
+
+    const handleCarouselClick = (index) => {
+        serActiveSelection(index);
+    };
+
+    const getStyle = (index) => {
+        if (activeSelection === index) {
+            return {
+                display: 'flex',
+                animation: 'slideInFromRight 0.8s ease-out'
+            };
+        }
+        return { display: 'none' };
+    };
 
   return (
     <>
         <header id="home">
             <h3>Project Showcase</h3>
             <div className="header__project-container">
-                <section className="header__section-home">
+                <section className="header__section-home" style={getStyle(0)}>
                     <p>Select a project from below for a preview</p>
                     <p>Projects are also listed further down</p>
                 </section>
-                <section className="header__project-whitespace">
+                <section className="header__project-whitespace" style={getStyle(1)}>
                     <p>The Whitespace Saga</p>
                     <div className="floating-dot"></div>
                     <div className="floating-dot"></div>
@@ -56,18 +50,18 @@ function Header() {
                     <div className="floating-dot"></div>
                 </section>
 
-                <section className="header__project-snapshot">
+                <section className="header__project-snapshot" style={getStyle(2)}>
                     <p>Snapshot Journey</p>
                 </section>
 
-                <section className="header__project-insanity">
+                <section className="header__project-insanity" style={getStyle(3)}>
                     <p>CSS Insanity</p>
                 </section>
             </div>
             <section className="header__project-nav">
-                <button>The Whitespace Saga</button>
-                <button>Snapshot Journey</button>
-                <button>CSS Insanity</button>
+                <button onClick={() => handleCarouselClick(1)}>The Whitespace Saga</button>
+                <button onClick={() => handleCarouselClick(2)}>Snapshot Journey</button>
+                <button onClick={() => handleCarouselClick(3)}>CSS Insanity</button>
             </section>
         </header>
     </>
