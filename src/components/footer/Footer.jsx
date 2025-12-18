b46220584acca93df069a4add7883ad75763f1e5
Ruben Ellefsen <ellefsen88@gmail.com>
2025-12-18 14:26:46 +0100

    Add email and some styling to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,12 @@ function Footer() {
   return (
     <>
         <footer id="contact">
+          <h3>Contact</h3>
+          <div>
+            <a href="mailto:rubell388@uib.no">Email</a>
             <a href="https://www.linkedin.com/in/ruben-nienhuis/" rel="noopener noreferrer" target="_blank">LinkedIn</a>
+          </div>
+          <p>Created by Ruben Nienhuis</p>
         </footer>
     </>
   )
