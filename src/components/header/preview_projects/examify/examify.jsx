f66cf71521a93402f36752d6c029b3e773496b32
Ruben Ellefsen <ellefsen88@gmail.com>
2025-12-18 14:26:01 +0100

    Add examify to as project

---
+++ src/components/header/preview_projects/examify/examify.jsx
@@ -0,0 +1,19 @@
+import './examify.css'
+
+function Examify() {
+
+  return (
+    <>
+        <div className="section__examify--container">
+            <h1>Klar for eksamen?</h1>
+            <h1> Test deg her!</h1>
+            <p>
+                Test kunnskapen din med tidligere eksamensoppgaver, og se hva du må lese mer på.
+            </p>
+            <a href='https://examify1.vercel.app/'>Start her</a>
+        </div>
+    </>
+  )
+}
+
+export default Examify
